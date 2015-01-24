7bde7c29e4521090473c0d298f72e7aaace83605
desarrollo <desarrollo@localhost.localdomain>
2015-01-24 17:18:56 +0100

    commit

--- c2/e1_servidor.py
+++ c2/e1_servidor.py
@@ -9,10 +9,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    """
+    Controlador para el directorio principal.
+    Por cada codigo, se genera una tupla codigo, resultados
+    codigo lista de cadenas que son las lineas de un codigo
+    resultado es una lista con los resultados de la ejecucion del anterior codigo
+    """
+    # codigo es una lista de cadenas que representan lineas de codigo
     codigo = get_source_lines(c2.e1_servidor)
+    
+    # resultado es una lista con los resultados obtenidos en la ejecucion del codigo anterior
     resultado = list()
+    
+    # results es una lista de pares codigo, resultado
+    # la plantilla recogera pares de codigo,resultados y los pintara
     results = [(codigo, resultado)]
-    return render_template("python_printer.html", template_vars={'results': results})
+    return render_template("c2_servidor.html", template_vars={'results': results})
 
 if __name__ == '__main__':
     app.debug = True
--- servidor.py
+++ servidor.py
@@ -3,6 +3,7 @@ __author__ = 'jesus.pedro.gutierrez.almazan'
 from flask import Flask
 from c2 import e1_servidor
 from tools.utils import get_source_lines
+from c4 import e1_flujo, e2_flujo, e3_flujo
 from flask.templating import render_template
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -13,7 +14,29 @@ def hello_world():
     codigo = get_source_lines(e1_servidor)
     resultado = list()
     results = [(codigo, resultado)]
-    return render_template("python_printer.html", template_vars={'results': results})
+    return render_template("c2_servidor.html", template_vars={'results': results})
+
+@app.route('/c4/e1')
+def c4_e1_flujo():
+    codigo = get_source_lines(e1_flujo)
+    resultado = list()
+    results = [(codigo, resultado)]
+    return render_template("c2_servidor.html", template_vars={'results': results})
+
+@app.route('/c4/e2')
+def c4_e2_flujo():
+    codigo = get_source_lines(e2_flujo)
+    resultado = list()
+    results = [(codigo, resultado)]
+    return render_template("c2_servidor.html", template_vars={'results': results})
+
+@app.route('/c4/e3')
+def c4_3_flujo():
+    codigo = get_source_lines(e3_flujo)
+    resultado = list()
+    results = [(codigo, resultado)]
+    return render_template("c2_servidor.html", template_vars={'results': results})
+
 
 if __name__ == '__main__':
     app.debug = True
