5db9f3ae9c342c9d808b4cd033d9d686d35fe396
desarrollo <desarrollo@localhost.localdomain>
2015-01-24 15:46:35 +0100

    commit

--- c2/e1_servidor.py
+++ c2/e1_servidor.py
@@ -1,19 +1,21 @@
-from flask import Flask
-from flask_debugtoolbar import DebugToolbarExtension
-
 __author__ = 'jesus.pedro.gutierrez.almazan'
+import c2.e1_servidor
+from flask import Flask
+from tools import get_source_lines
+from flask.templating import render_template
+from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    codigo = get_source_lines(c2.e1_servidor)
+    resultado = list()
+    results = [(codigo, resultado)]
+    return render_template("python_printer.html", template_vars={'results': results})
 
 if __name__ == '__main__':
-    
     app.debug = True
     app.config['SECRET_KEY'] = 'AhLae9chahKua5fuZia7ooji'
-    
     toolbar = DebugToolbarExtension(app)
-
     app.run(host="0.0.0.0", port=5000)
