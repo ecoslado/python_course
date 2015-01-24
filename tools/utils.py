3a900db9fe82a57ffc4dee6e67a68b1651a60339
desarrollo <desarrollo@localhost.localdomain>
2015-01-24 17:31:02 +0100

    añadida docstring de la funcion get_source_lines

--- tools/utils.py
+++ tools/utils.py
@@ -1,5 +1,10 @@
 __author__ = 'jesus.pedro.gutierrez.almazan'
 import inspect
 
-def get_source_lines(func):
-    return inspect.getsourcelines(func)[0]
+def get_source_lines(obj):
+    """
+    Obtiene las lineas de codigo de un objeto, ya sea una funcion, modulo, frame, ...
+    @param obj: Objeto del que obtener las lineas de codigo
+    @return: Una lista de cadenas que son las lineas de codigo del objeto
+    """
+    return inspect.getsourcelines(obj)[0]
